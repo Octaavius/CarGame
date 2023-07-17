e5cf1177291b3c9b8ac874bc19acb60418c216e9
Andrii <andpokora@gmail.com>
2023-07-17 11:10:52 +0300

    GameManager script cleaned
    added function spawnCar()

--- Car Game/Assets/Scripts/GameManager.cs
+++ Car Game/Assets/Scripts/GameManager.cs
@@ -7,40 +7,29 @@ public class GameManager : MonoBehaviour
 {
     public GameObject spawnPoint;
     public GameObject carParkObj;
+    private CarPark carParkScr;
+
+    private GameObject currentCar;
     public GameObject carCamera;
-    public GameObject currentCar;
     public GameObject UI;
 
     void Start(){
-        CarPark carParkScr = carParkObj.GetComponent<CarPark>();
-        currentCar = Instantiate(currentCar, spawnPoint.transform.position, Quaternion.identity);
-        
-        Follow followScript = carCamera.GetComponent<Follow>();
-        followScript.Target = currentCar;
-        followScript.setCameraPosition(currentCar.transform.Find("Pos1"), currentCar.transform.Find("Pos2"));  
-        
-        PhoneCameraRotate phoneCameraScript = carCamera.GetComponent<PhoneCameraRotate>();
-        phoneCameraScript.Target = currentCar.transform;
+        Application.targetFrameRate = -1;
 
-        Tachometer tachometerScr = UI.transform.Find("Canvas/meters/Tachometer").GetComponent<Tachometer>();
-        tachometerScr.car = currentCar;
-
-        Speedometer speedometerScr = UI.transform.Find("Canvas/meters/Speedometer").GetComponent<Speedometer>();
-        speedometerScr.car = currentCar;
-
-        Button reverseButton = UI.transform.Find("Canvas/GearButtons/ReverseButton").GetComponent<Button>();
-        reverseButton.onClick.AddListener(() => currentCar.GetComponent<CarScript>().TurnOnReverse());
-
-        Button driveButton = UI.transform.Find("Canvas/GearButtons/DriveButton").GetComponent<Button>();
-        driveButton.onClick.AddListener(() => currentCar.GetComponent<CarScript>().TurnOnDrive());
+        carParkScr = carParkObj.GetComponent<CarPark>();
+        currentCar = carParkScr.carList[0];
+        spawnCarFromCarPark(0);
     }
 
     public void ChangeCar(){
         Destroy(currentCar);
-        CarPark carParkScr = carParkObj.GetComponent<CarPark>();
         
         int carId = (carParkScr.carList.Length == carParkScr.lastCarId + 1) ? carParkScr.lastCarId = 0 : ++carParkScr.lastCarId;
 
+        spawnCarFromCarPark(carId);
+    }
+
+    public void spawnCarFromCarPark(int carId){
         currentCar = Instantiate(carParkScr.carList[carId], spawnPoint.transform.position, Quaternion.identity);
         
         Follow followScript = carCamera.GetComponent<Follow>();

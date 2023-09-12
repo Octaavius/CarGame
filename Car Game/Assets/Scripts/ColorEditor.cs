f6a4308cb7172284142535f47223bf093de4cd5a
Andrii <andpokora@gmail.com>
2023-09-12 11:49:25 +0300

    bmw x6 color picker added

--- Car Game/Assets/Scripts/ColorEditor.cs
+++ Car Game/Assets/Scripts/ColorEditor.cs
@@ -13,17 +13,20 @@ namespace HSVPickerExamples
 	    // Use this for initialization
 	    void Start () 
         {
-            picker = GameObject.FindGameObjectWithTag("ColorPicker").GetComponent<ColorPicker>();
             gm = GameObject.FindGameObjectWithTag("gameManager").GetComponent<GameManager>();
             Color = renderer[0].material.color;
-            picker.onValueChanged.AddListener(color =>
+            if (GameObject.FindGameObjectWithTag("ColorPicker"))
             {
-                changeColor(color);
-                Color = color;
-                gm.carsColor[gm.lastCarId] = color;
-            });
+                picker = GameObject.FindGameObjectWithTag("ColorPicker").GetComponent<ColorPicker>();
+                picker.onValueChanged.AddListener(color =>
+                {
+                    changeColor(color);
+                    Color = color;
+                    gm.carsColor[gm.lastCarId] = color;
+                });
 
-		    picker.CurrentColor = Color;
+                picker.CurrentColor = Color;
+            }
         }
 
         public void changeColor(Color color){
